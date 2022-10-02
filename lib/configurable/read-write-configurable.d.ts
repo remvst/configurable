e9ee221d4db9ac280dcb2105ec685f90a7b848f8
remvst <remvst@gmail.com>
2022-10-02 17:15:45 -0400

    Build

--- lib/configurable/read-write-configurable.d.ts
+++ lib/configurable/read-write-configurable.d.ts
@@ -1,9 +1,11 @@
 import Configurable from './configurable';
 export default class ReadWriteConfigurable<ValueType> extends Configurable {
-    private readonly read;
-    private readonly write;
+    private readonly doRead;
+    private readonly doWrite;
     constructor(opts: {
         readonly read: (configurable: Configurable) => ValueType;
         readonly write: (value: ValueType, configurable: Configurable) => void;
     });
+    read(): ValueType;
+    write(value: ValueType): void;
 }
